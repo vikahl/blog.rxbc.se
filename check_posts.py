bd06b333fb3ef1ca2bc17313220266dd7a4ed2cc
Viktor Rimark <viktor@rxbc.se>
2023-11-15 20:44:32 +0100

    Add script to check post metadata
    Prevent accidentally merging posts without updating the post date or
    disable draft.

---
+++ check_posts.py
@@ -0,0 +1,40 @@
+import datetime
+import re
+import subprocess
+import os
+
+# Get added files in current branch
+added_files = subprocess.run(
+    ["git", "diff", "--name-only", "--diff-filter=A", "origin/main", "HEAD"],
+    encoding="utf-8",
+    stdout=subprocess.PIPE,
+    check=True,
+).stdout
+
+
+for filename in added_files.split("\n"):
+    if filename.startswith("content/posts/"):
+        with open(filename) as fh:
+            # Ugly, but easy. Rather than creating some clever regex that will
+            # only match the metdata within "---" and "---", just read the
+            # first 10 lines and be happy with it.
+            metadata_block = "".join([next(fh) for _ in range(10)])
+
+            print(metadata_block)
+            metadata = re.findall("(\w+):\s*(.*)", metadata_block, flags=re.M)
+
+        for line in metadata:
+            key, value = line
+
+            # checks
+            if key == "draft" and value.lower() == "true":
+                raise RuntimeError(
+                    "Draft is enabled. It should be disabled before merge!"
+                )
+
+            if key == "date":
+                # Try to load it as a datetime:
+                date = datetime.datetime.fromisoformat(value)
+
+                if date.today() != date:
+                    raise RuntimeError(f"Date is too old: {date}")
